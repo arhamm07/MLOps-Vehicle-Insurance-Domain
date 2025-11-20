6a907e102c18eaa602908c29ce92df17cf2090dd
arham <arhamop937@gmail.com>
2025-11-20 14:59:04 +0500

    Project metadata added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setup import setup, find_packages
+
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="M Arham Rafique",
+    author_email="arhammrafique@gmail.com",
+    packages=find_packages()
+)
